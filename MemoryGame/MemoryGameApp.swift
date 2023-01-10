2c64244473ef2e326b041b8fcb69504496c52745
g-ruffo <grayson@veltus.ca>
2023-01-09 17:28:10 -0800

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Grayson Ruffo on 2023-01-09.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
