2110aed312bc3b54b444b2d18fa070b3bd7a63ce
g-ruffo <grayson@veltus.ca>
2023-01-11 00:30:15 -0800

    feat: Implement viewmodel as an observed object

--- MemoryGame/MemoryGameModel.swift
+++ MemoryGame/MemoryGameModel.swift
@@ -11,10 +11,23 @@ struct MemoryGameModel<CardContent> {
     // Set private(set) to prevent changes to the variable from outside this scope. Makes it read-only.
     private(set) var cards: Array<Card>
     
-    func choose(_ card: Card) {
-        
+    // Mark function as mutating to allow object variable to be altered.
+    mutating func choose(_ card: Card) {
+        let chosenIndex = index(of: card)
+        cards[chosenIndex].isFaceUp.toggle()
+        print("chosen card = \(cards[chosenIndex])")
+        print("\(cards)")
+
     }
     
+    func index(of card: Card) -> Int {
+        for index in 0..<cards.count {
+            if cards[index].id == card.id {
+                return index
+            }
+        }
+        return 0
+    }
     
     init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
         cards = Array<Card>()
