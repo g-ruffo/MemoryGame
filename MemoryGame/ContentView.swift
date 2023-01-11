0a7d2cf41bbb5f14f22845d296fea8eb08365560
g-ruffo <grayson@veltus.ca>
2023-01-10 21:03:40 -0800

    feat: Remove buttons

--- MemoryGame/ContentView.swift
+++ MemoryGame/ContentView.swift
@@ -17,51 +17,18 @@ struct ContentView: View {
             
             ScrollView {
                 LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
-                    
                     ForEach(emojies[0..<emojiCount], id: \.self) { emoji in
                         CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
-                        
                     }
                 }
-                .padding(.horizontal)
                 .foregroundColor(.red)
             }
             
-            Spacer()
-            
-            HStack {
-                
-                remove
-                
-                Spacer()
-            
-                add
-            }
             .font(.largeTitle)
             .padding(.horizontal)
         }
     }
-    
-    var add: some View {
-        Button {
-            if emojiCount < emojies.count {
-                emojiCount += 1
-            }
-        } label: {
-            Image(systemName: "plus.circle")
-        }
-    }
-    
-    var remove: some View {
-        Button {
-            if emojiCount > 1 {
-                emojiCount -= 1
-            }
-            
-        } label: {
-            Image(systemName: "minus.circle")
-        }
-    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
