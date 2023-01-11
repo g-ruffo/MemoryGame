eedb97b18126f1523c79f3ac3d00bdb0702550d1
g-ruffo <grayson@veltus.ca>
2023-01-10 23:03:13 -0800

    feat: Create init function to generate cards

--- MemoryGame/MemoryGameModel.swift
+++ MemoryGame/MemoryGameModel.swift
@@ -8,15 +8,27 @@
 import Foundation
 
 struct MemoryGameModel<CardContent> {
-    var cards: Array<Card>
+    // Set private(set) to prevent changes to the variable from outside this scope. Makes it read-only.
+    private(set) var cards: Array<Card>
     
     func choose(_ card: Card) {
         
     }
     
+    
+    init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
+        cards = Array<Card>()
+        // Add numberOfPairsOfCards x 2 cards to cards array.
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = createCardContent(pairIndex)
+            cards.append(Card(content: content))
+            cards.append(Card(content: content))
+        }
+    }
+    
     struct Card {
-        var isFaceUp: Bool
-        var isMatched: Bool
+        var isFaceUp: Bool = false
+        var isMatched: Bool = false
         var content: CardContent
     }
 }
