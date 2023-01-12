e64dfd623040ea3731a2734a3cd534d9c06137fe
g-ruffo <grayson@veltus.ca>
2023-01-11 19:00:26 -0800

    feat: Hide card if matched

--- MemoryGame/ContentView.swift
+++ MemoryGame/ContentView.swift
@@ -49,6 +49,8 @@ struct CardView: View {
                 shape.fill().foregroundColor(Color.white)
                 shape.strokeBorder(lineWidth: 3)
                 Text(card.content).font(.largeTitle)
+            } else if card.isMatched {
+                shape.opacity(0)
             } else{
                 shape.fill()
             }
