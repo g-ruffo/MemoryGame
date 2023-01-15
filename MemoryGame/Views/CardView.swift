8d1b6c4e8fb10118720b856741004b65129a4dda
g-ruffo <grayson@veltus.ca>
2023-01-14 23:48:57 -0800

    refactor: Move files to corresponding folder

---
+++ MemoryGame/Views/CardView.swift
@@ -0,0 +1,32 @@
+//
+//  CardView.swift
+//  MemoryGame
+//
+//  Created by Grayson Ruffo on 2023-01-14.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let card: MemoryGameModel<String>.Card
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
+                if card.isFaceUp {
+                    shape.fill().foregroundColor(Color.white)
+                    shape.strokeBorder(lineWidth: DrawingConstants.strokeCardBorder)
+                    Text(card.content).font(font(in: geometry.size))
+                } else if card.isMatched {
+                    shape.opacity(0)
+                } else{
+                    shape.fill()
+                }
+            }
+        }
+    }
+    
+    private func font(in size: CGSize) -> Font {
+        Font.system(size: min(size.width, size.height) * DrawingConstants.fontScale)
+    }
+}
