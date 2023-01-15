2e1db048c35afe7c35a85631cb57ea410929ccaf
g-ruffo <grayson@veltus.ca>
2023-01-15 01:34:53 -0800

    style: Change emoji size

--- MemoryGame/Values/DrawingConstants.swift
+++ MemoryGame/Values/DrawingConstants.swift
@@ -10,6 +10,6 @@ import Foundation
 struct DrawingConstants {
     static let cornerRadius: CGFloat = 10
     static let strokeCardBorder: CGFloat = 3
-    static let fontScale: CGFloat = 0.7
+    static let fontScale: CGFloat = 0.6
     static let circleTimerOpacity: CGFloat = 0.5
 }
