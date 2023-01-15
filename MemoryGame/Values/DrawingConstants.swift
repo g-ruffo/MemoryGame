ed8c0ac9b5e55c16ccba65f9867da6657cc4cdf7
g-ruffo <grayson@veltus.ca>
2023-01-15 01:18:11 -0800

    refactor: Move constants to file

---
+++ MemoryGame/Values/DrawingConstants.swift
@@ -0,0 +1,15 @@
+//
+//  DrawingConstants.swift
+//  MemoryGame
+//
+//  Created by Grayson Ruffo on 2023-01-15.
+//
+
+import Foundation
+
+struct DrawingConstants {
+    static let cornerRadius: CGFloat = 10
+    static let strokeCardBorder: CGFloat = 3
+    static let fontScale: CGFloat = 0.7
+    static let circleTimerOpacity: CGFloat = 0.5
+}
