7580a6fc13fde181dd99218c10069667fd82b3ea
g-ruffo <grayson@veltus.ca>
2023-01-14 21:06:14 -0800

    style: Update card sizing

--- MemoryGame/EmojiMemoryGameView.swift
+++ MemoryGame/EmojiMemoryGameView.swift
@@ -19,6 +19,8 @@ struct EmojiMemoryGameView: View {
                 Spacer()
                 Text(viewModel.themeName).font(.largeTitle)
                 Spacer()
+                Spacer()
+
                 Text("\(viewModel.score)").font(.largeTitle)
                 Spacer()
             }
@@ -26,7 +28,7 @@ struct EmojiMemoryGameView: View {
             Spacer()
             
             ScrollView {
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))]) {
                     ForEach(viewModel.cards) { card in
                         CardView(card: card)
                             .aspectRatio(2/3, contentMode: .fit)
@@ -63,18 +65,29 @@ struct CardView: View {
     let card: MemoryGameModel<String>.Card
     
     var body: some View {
-        ZStack {
-            let shape = RoundedRectangle(cornerRadius: 20)
-            if card.isFaceUp {
-                shape.fill().foregroundColor(Color.white)
-                shape.strokeBorder(lineWidth: 3)
-                Text(card.content).font(.largeTitle)
-            } else if card.isMatched {
-                shape.opacity(0)
-            } else{
-                shape.fill()
+        GeometryReader { geometry in
+            ZStack {
+                let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
+                if card.isFaceUp {
+                    shape.fill().foregroundColor(Color.white)
+                    shape.strokeBorder(lineWidth: DrawingConstants.strokeCardBorder)
+                    Text(card.content).font(font(in: geometry.size))
+                } else if card.isMatched {
+                    shape.opacity(0)
+                } else{
+                    shape.fill()
+                }
             }
         }
     }
     
+    private func font(in size: CGSize) -> Font {
+        Font.system(size: min(size.width, size.height) * DrawingConstants.fontScale)
+    }
+}
+
+private struct DrawingConstants {
+    static let cornerRadius: CGFloat = 20
+    static let strokeCardBorder: CGFloat = 3
+    static let fontScale: CGFloat = 0.8
 }
