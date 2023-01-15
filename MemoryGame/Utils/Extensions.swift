980d2b9f2ba499b05cb1ebdb7336223ddf35a4dc
g-ruffo <grayson@veltus.ca>
2023-01-15 14:59:31 -0800

    feat: Create extension for view modifier

--- MemoryGame/Utils/Extensions.swift
+++ MemoryGame/Utils/Extensions.swift
@@ -6,10 +6,17 @@
 //
 
 import Foundation
+import SwiftUI
 
 
 extension Array {
     var oneAndOnly: Element? {
         self.count == 1 ? self.first : nil
+    }
+}
+
+extension View {
+    func cardify(isFaceUp: Bool) -> some View {
+        return self.modifier(Cardify(isFaceUp: isFaceUp))
     }
 }
