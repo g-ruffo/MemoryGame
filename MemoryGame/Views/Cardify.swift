d410456095f4efee9a5c952563ac82ef6b41b10b
g-ruffo <grayson@veltus.ca>
2023-01-15 14:58:47 -0800

    feat: Create view modifier

---
+++ MemoryGame/Views/Cardify.swift
@@ -0,0 +1,28 @@
+//
+//  Cardify.swift
+//  MemoryGame
+//
+//  Created by Grayson Ruffo on 2023-01-15.
+//
+
+import SwiftUI
+
+struct Cardify: ViewModifier {
+    
+    var isFaceUp: Bool
+    
+    func body(content: Content) -> some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
+            if isFaceUp {
+                shape.fill().foregroundColor(Color.white)
+                shape.strokeBorder(lineWidth: DrawingConstants.strokeCardBorder)
+                content
+            } else{
+                shape.fill()
+            }
+        }
+    }
+}
+
+
