138746a5989d20011a5a1024aa2f1ca0021c4913
g-ruffo <grayson@veltus.ca>
2023-01-14 18:52:01 -0800

    fix: Update negative points

--- MemoryGame/MemoryGameModel.swift
+++ MemoryGame/MemoryGameModel.swift
@@ -10,16 +10,36 @@ import Foundation
 struct MemoryGameModel<CardContent> where CardContent: Equatable {
     // Set private(set) to prevent changes to the variable from outside this scope. Makes it read-only.
     private(set) var cards: Array<Card>
-    
     private var indexOfTheOnlyFaceupCard: Int?
+    
+    //    private var indexOfTheOnlyFaceupCard: Int? {
+    //        get { cards.indices.filter { cards[$0].isFaceUp }.oneAndOnly }
+    //        set {
+    //
+    //            cards.indices.forEach {
+    //                cards[$0].hasBeenSeen = cards[$0].isFaceUp && ($0 != newValue)
+    //                cards[$0].isFaceUp = ($0 == newValue)
+    //            }
+    ////            for index in cards.indices {
+    ////                if index != newValue {
+    ////                    if cards[index].isFaceUp {
+    ////                        cards[index].isFaceUp = false
+    ////                        cards[index].hasBeenSeen = true
+    ////                    }
+    ////                } else {
+    ////                    cards[index].isFaceUp = true
+    ////                }
+    ////            }
+    //        }
+    //    }
     
     private(set) var score = 0
     
     // Mark function as mutating to allow object variable to be altered.
     mutating func choose(_ card: Card) {
         if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }),
-            !cards[chosenIndex].isFaceUp,
-            !cards[chosenIndex].isMatched {
+           !cards[chosenIndex].isFaceUp,
+           !cards[chosenIndex].isMatched {
             
             if let potentialMatchIndex = indexOfTheOnlyFaceupCard {
                 if cards[chosenIndex].content == cards[potentialMatchIndex].content {
@@ -32,6 +52,7 @@ struct MemoryGameModel<CardContent> where CardContent: Equatable {
                     }
                 }
                 indexOfTheOnlyFaceupCard = nil
+
             } else {
                 for index in cards.indices {
                     if cards[index].isFaceUp {
@@ -62,5 +83,11 @@ struct MemoryGameModel<CardContent> where CardContent: Equatable {
         var hasBeenSeen: Bool = false
         let content: CardContent
         let id: Int
+    }
+}
+
+extension Array {
+    var oneAndOnly: Element? {
+        self.count == 1 ? self.first : nil
     }
 }
