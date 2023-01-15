c88fc5caca4069240400736800911dec59b48ab7
g-ruffo <grayson@veltus.ca>
2023-01-15 01:20:30 -0800

    feat: Create timer circle

---
+++ MemoryGame/Views/Pie.swift
@@ -0,0 +1,23 @@
+//
+//  Pie.swift
+//  MemoryGame
+//
+//  Created by Grayson Ruffo on 2023-01-15.
+//
+
+import SwiftUI
+
+struct Pie: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        var p = Path()
+        
+        return p
+    }
+}
+
+struct Pie_Previews: PreviewProvider {
+    static var previews: some View {
+        Pie()
+    }
+}
