0632be3f3ea8e80ed916f0083254004015fd9fda
g-ruffo <grayson@veltus.ca>
2023-01-15 23:44:07 -0800

    feat: Add timer variables to Card

--- MemoryGame/Data/Card.swift
+++ MemoryGame/Data/Card.swift
@@ -8,9 +8,55 @@
 import Foundation
 
 struct Card<CardContent>: Identifiable where CardContent: Equatable {
-    var isFaceUp: Bool = false
-    var isMatched: Bool = false
+    var isFaceUp: Bool = false {
+        didSet {
+            if isFaceUp {
+                startUsingBonusTime()
+            } else {
+                stopUsingBonusTime()
+            }
+        }
+    }
+    var isMatched: Bool = false {
+        didSet {
+            stopUsingBonusTime()
+        }
+    }
     var hasBeenSeen: Bool = false
     let content: CardContent
     let id: Int
+    
+    
+    var bonusTimeLimit: TimeInterval = DrawingConstants.bonusTimeLimit
+    private var faceUpTime: TimeInterval {
+        if let lastFaceUpDate = self.lastFaceUpDate {
+            return pastFaceUpTime + Date().timeIntervalSince(lastFaceUpDate)
+        } else {
+            return pastFaceUpTime
+        }
+    }
+    var lastFaceUpDate: Date?
+    var pastFaceUpTime: TimeInterval = 0
+    var bonusTimeRemaining: TimeInterval {
+        max(0, bonusTimeLimit - faceUpTime)
+    }
+    var bonusRemaining: Double {
+        (bonusTimeLimit > 0 && bonusTimeRemaining > 0) ? bonusTimeRemaining / bonusTimeLimit : 0
+    }
+    var hasEarnedBonus: Bool {
+        isFaceUp && !isMatched && bonusTimeRemaining > 0
+    }
+    var isConsumingBonusTime: Bool {
+        isFaceUp && !isMatched && bonusTimeRemaining > 0
+    }
+    private mutating func startUsingBonusTime( ) {
+        if isConsumingBonusTime, lastFaceUpDate == nil {
+            lastFaceUpDate = Date()
+        }
+    }
+    private mutating func stopUsingBonusTime( ) {
+        pastFaceUpTime = faceUpTime
+        self.lastFaceUpDate = nil
+    }
+    
 }
